828cce0a879cc65213464e6a9dfdd41d45bdde43
Louis de Monterno <louisdemonterno@protonmail.com>
2024-02-25 12:30:33 +0100

    adding remaining words counter in main screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'names.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,6 +29,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _numCivilians = 1;
   int _numUndercovers = 1;
+  int _totalWords = 0;
+  int _usedWords = 0;
 
   // this function is called when the user validates the number of players
   void _navigateToNameSelection() {
@@ -46,8 +49,23 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  void getNbWords() async {
+    final prefs = await SharedPreferences.getInstance();
+    _usedWords = prefs.getInt('counter') ?? -1;
+    _usedWords ++;
+    try {
+      final csvData = await loadAsset();
+      List<String> rows = csvData.split('\n');
+      _totalWords = rows.length;
+    } catch (e) {
+      print('Error reading CSV: $e');
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    getNbWords();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Role-playing Game'),
@@ -56,6 +74,8 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text('number of pair of words used: $_usedWords / $_totalWords.'),
+            const SizedBox(height: 20),
             Text('Number of Civilians'),
             Slider(
               value: _numCivilians.toDouble(),
